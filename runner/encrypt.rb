19c86a926079904723e4f165b9efa94ddc0df56c
David Lopez <ldavid.x.lopezl@gmail.com>
2019-09-15 14:32:28 -0600

    Fix taking in files as input

--- runner/encrypt.rb
+++ runner/encrypt.rb
@@ -1,14 +1,14 @@
 require './lib/enigma'
 
 @enigma = Enigma.new
-@file = File.open('./message.txt', 'r')
+@file = File.open(ARGV[0], 'r')
 
 def write
   encryption_hash = @enigma.encrypt(@file.read.chomp)
-  new_file = File.open('./encrypted.txt', 'w')
+  new_file = File.open(ARGV[1], 'w')
   new_file.write(encryption_hash[:encryption])
 
-  print "Created 'encrypted.txt' "
+  print "Created '#{ARGV[1]}' "
   print "with the key #{encryption_hash[:key]} and date "
   puts encryption_hash[:date]
 end
