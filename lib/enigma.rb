58bd489b980e4166d03227e36016d9e93057f3ef
David Lopez <ldavid.x.lopezl@gmail.com>
2019-09-15 14:40:32 -0600

    Fix date to be ddmmyy

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,9 +5,9 @@ class Enigma < Decryption
     @message_decryption = Decryption.new
   end
 
-  def encrypt(message, key = '', date = Time.now.strftime("%m%d%y"))
+  def encrypt(message, key = '', date = Time.now.strftime("%d%m%y"))
     date, key = key, rand.to_s[2..6] if key.length != 5
-    date = Time.now.strftime("%m%d%y") if date.length != 6
+    date = Time.now.strftime("%d%m%y") if date.length != 6
     {
       encryption: @message_encryption.message(message, key, date),
       key: key,
@@ -15,7 +15,7 @@ class Enigma < Decryption
     }
   end
 
-  def decrypt(message, key, date = Time.now.strftime("%m%d%y"))
+  def decrypt(message, key, date = Time.now.strftime("%d%m%y"))
     {
       decryption: @message_decryption.message(message, key, date),
       key: key,
