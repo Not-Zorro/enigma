dbb159ca06b493afcd6f5287bc7bed54498a9f45
David Lopez <ldavid.x.lopezl@gmail.com>
2019-09-15 09:43:12 -0600

    Message alters into new message and pass test

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -8,5 +8,13 @@ class Encryption
 
   def message(message, key, date)
     @shift = final_shift(key, date)
+    format_message = message.downcase.chars
+    altered_message = format_message.map do |character|
+      letter_location = (character.ord - 97 + @shift[0]) % 27
+      value = @alphabet[letter_location]
+      @shift.rotate!
+      value
+    end
+    altered_message.join
   end
 end
