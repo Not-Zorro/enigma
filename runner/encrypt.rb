3cda1baacd64cfec530125a04a89745590ba55cb
David Lopez <ldavid.x.lopezl@gmail.com>
2019-09-15 14:16:02 -0600

    Add file i/o and runners

---
+++ runner/encrypt.rb
@@ -0,0 +1,15 @@
+require './lib/enigma'
+
+@enigma = Enigma.new
+@file = File.open('./message.txt', 'r')
+
+def write
+  encryption_hash = @enigma.encrypt(@file.read.chomp)
+  new_file = File.open('./encrypted.txt', 'w')
+  new_file.write(encryption_hash[:encryption])
+  print "Created 'encrypted.txt' "
+  print "with the key #{encryption_hash[:key]} and date "
+  puts encryption_hash[:date]
+end
+
+write
